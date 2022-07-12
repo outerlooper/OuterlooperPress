e74f36cc6cac8db97ba0dc9f023cf28c77734a98
Michael Cooper <outerlooper@gmail.com>
2022-07-12 10:37:02 -0500

    Hello, Love

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser'); 
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+
+
+
+// exports.default = gulp.series(gulp.parallel(
+//     pgPortfolio, html, icons, images, fonts, vendor, css, cssMin, js, jsMin
+//     ), watch);
