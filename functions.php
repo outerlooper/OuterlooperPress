f15b3129854a9a107573f06590e8cca6e0e8d895
Mike Cooper <mgcooper@UT518007.local>
2022-07-22 20:56:09 -0500

    Wire-up theme functions, styles and scripts

--- functions.php
+++ functions.php
@@ -1,12 +1,29 @@
 <?php
 
-/* Function to enqueue stylesheet from parent theme */
+/* OuterlooperPress Functions */
 
-function child_enqueue__parent_scripts() {
-
-wp_enqueue_style( ‘parent’, get_template_directory_uri().’/style.css’ );
-
+add_action( 'wp_enqueue_scripts', 'parent_theme_enqueue_styles' );
+function parent_theme_enqueue_styles() {
+    $parenthandle = 'twentytwentyone'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
 }
 
-add_action( ‘wp_enqueue_scripts’, ‘child_enqueue__parent_scripts’);
+add_action( 'wp_enqueue_scripts', 'OuterlooperPress_enqueue_styles', 818 );
+function OuterlooperPress_enqueue_styles() {
+    $parenthandle = 'twentytwentyone'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( 'outerlooper-press-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'OuterlooperPress_enqueue_scripts', 818 );
+function OuterlooperPress_enqueue_scripts() {
+    wp_enqueue_script( 'outerlooper-press-scripts', get_stylesheet_directory_uri() . '/scripts.js');
+}
 ?>
--- header.php
+++ header.php
@@ -30,3 +30,5 @@
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main">
+
+				
